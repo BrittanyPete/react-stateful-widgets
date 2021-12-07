f2c135a7cb8b7107459fd6c10d230b02fa4b7a29
Brittany Petersen <britt.petersen100@gmail.com>
2021-12-06 17:22:38 -0700

    completed code for programmers.js and spinner.js

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -27,8 +27,8 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-  const [programmers, setProgrammers] = useState(listOfAwesome);
-  const [id, setID] = useState('');
+  const [programmers] = useState(listOfAwesome);
+  const [id, setID] = useState(null);
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -36,7 +36,7 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-
+  return programmers.find(dev => dev.id === id).name;
        
   };
 
